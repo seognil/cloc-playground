4b37bc0cd94d7d43537ed43737b2edc518af038b
seognil LC <seognil@gmail.com>
2020-02-08 15:57:50 +0800

    feat(file): add test files

---
+++ different-name/1.js
@@ -0,0 +1,13 @@
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+//
+//
---
+++ different-name/4.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function 1() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
